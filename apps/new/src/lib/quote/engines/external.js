05b004d133dd5b67b622dffc18ce8b7ac20d571b
freepass-creator <dudguq@gmail.com>
2026-09-25 20:31:24 +0900

    feat(quote): bind PriceBasis to selected product

--- apps/new/src/lib/quote/engines/external.js
+++ apps/new/src/lib/quote/engines/external.js
@@ -36,7 +36,7 @@ export async function 계산(요청, { 신호 } = {}) {
     throw error;
   }
 
-  const priceBasis = normalizePriceBasis(j.priceBasis);
+  const priceBasis = normalizePriceBasis(j.priceBasis, { expectedProductId: 요청?.차?.상품키 || 요청?.차?.키 });
   return {
     차량가: j.vehiclePrice ?? null,
     priceBasis,
